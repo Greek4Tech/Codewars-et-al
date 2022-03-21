472a259b16b9084ff2d65a8e7aafae13e2c81847
Greek4Tech <leonidas.gkimisis@gmail.com>
2022-03-21 09:10:06 -0700

    Power of Two

--- Assets/JS.js
+++ Assets/JS.js
@@ -3,3 +3,11 @@ const flip=(d, a)=>{
     if(d === 'R') return a.sort((a,b)=>a-b);
     if(d === 'L') return a.sort((a,b)=>b-a);
   }
+
+  // Pow of two
+
+function powerOfTwo (n) {
+  for (let i=0; i<=n; i++) {
+    result.push(Math.pow(2,i))
+  } return result;
+}
