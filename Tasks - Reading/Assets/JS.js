329ed837f86642c82d35305b278510195f0c1041
Greek4Tech <leonidas.gkimisis@gmail.com>
2022-03-26 13:54:40 -0700

    Update JS.js

--- Tasks - Reading/Assets/JS.js
+++ Tasks - Reading/Assets/JS.js
@@ -1,7 +1,7 @@
 //filter out "the geese"
 
 function gooseFilter (birds) {
-  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  let geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
   return birds.filter(b => !geese.includes(b));
 };
 
