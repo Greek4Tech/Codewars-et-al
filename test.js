cb45560a1658e509410412fba035a3233c49fcad
Greek4Tech <leonidas.gkimisis@gmail.com>
2022-04-06 14:16:47 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
-function findMultiples(int,limit) {
-let res = []
-for(let i=int; i<=limit; i+=int) {
-    res.push(i)
+//remove first and last character 
+
+function removeStuff (str) {
+    return str.slice(1,-1)
 } 
-console.log(res) 
-}
+removeStuff("Original")
