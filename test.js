421e2b2b1b9f2ba5941e7dd0ef50d4d272e107c0
Greek4Tech <leonidas.gkimisis@gmail.com>
2022-04-06 10:51:46 -0700

    Create test.js

---
+++ test.js
@@ -0,0 +1,8 @@
+function consecutive (arr) {
+    for (i=0; i<=arr.length; i++) {
+        if (arr[i]+1 !== arr[i+1]) {
+            console.log(`${arr[i+1]} is not conse`)
+        }
+    } return null 
+
+}
