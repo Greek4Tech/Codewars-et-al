e5435e1ac4080c3179205ff249f5b9b58e52d61f
Greek4Tech <leonidas.gkimisis@gmail.com>
2022-04-11 12:43:09 -0700

    API

--- test.js
+++ test.js
@@ -1,10 +1,16 @@
-//every other index 
+// function multiples (num, limit) {
+//     let newArr = []
+//         for (i=num; i<=limit; i=i+num) {
+//             newArr.push(i)
+//         } console.log(newArr)
+// }
 
-function zeroOne (length) {
-    let arr = []
-        for (let i=0; i<=length; i++) {
-
-            arr = arr + i%2
-
-        } console.log(arr)
+function cascading (arr, num) {
+    let newArr = []
+        //loop
+            for (i=0; i<arr.length; i++) {
+                newArr.push(arr.slice(i, i+num))
+            } 
+        //restructure 
+        return newArr.filter(i, i.length === num)
 }
