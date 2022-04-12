fc33266bfb168814d392597f48f5af95c92b2718
Greek4Tech <leonidas.gkimisis@gmail.com>
2022-04-12 08:41:18 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,7 @@
-// function multiples (num, limit) {
-//     let newArr = []
-//         for (i=num; i<=limit; i=i+num) {
-//             newArr.push(i)
-//         } console.log(newArr)
-// }
-
-function cascading (arr, num) {
-    let newArr = []
-        //loop
-            for (i=0; i<arr.length; i++) {
-                newArr.push(arr.slice(i, i+num))
-            } 
-        //restructure 
-        return newArr.filter(i, i.length === num)
+function returnString (num) {
+    let str = ''
+        for (let i=1; i<=num; i++) {
+            str = str + i%2
+            return str
+        }
 }
