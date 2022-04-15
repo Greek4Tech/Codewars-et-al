8c14fbe154166c91655244ef72fd20c3598855ab
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-04-15 11:33:45 -0700

    Update test.js

---
+++ test.js
@@ -0,0 +1,39 @@
+new Promise(function(resolve, reject) {
+
+  setTimeout(() => resolve(1), 1000); // (*)
+
+}).then(function(result) { // (**)
+
+  alert(result); // 1
+  return result * 2;
+
+}).then(function(result) { // (***)
+
+  alert(result); // 2
+  return result * 2;
+
+}).then(function(result) {
+
+  alert(result); // 4
+  return result * 2;
+
+});
+
+let promise = new Promise(function(resolve, reject) {
+  setTimeout(() => resolve(1), 1000);
+});
+
+promise.then(function(result) {
+  alert(result); // 1
+  return result * 2;
+});
+
+promise.then(function(result) {
+  alert(result); // 1
+  return result * 2;s
+});
+
+promise.then(function(result) {
+  alert(result); // 1
+  return result * 2;
+});
