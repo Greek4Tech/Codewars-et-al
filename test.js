94b2d8ed839649d4779f7c443ed585cd01ec1d8d
Greek4Tech <leonidas.gkimisis@gmail.com>
2022-04-17 14:22:54 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,24 @@
-function multples (num1, num2) {
-    let result = []
-    for (let i=num1; i<=num2; i = num1 + num1){ 
-        result.push(i)
-        }
-        return result
+function zeroOne (num) {
+    let str = ''
+
+    for (let i=1; i<=num; i++) 
+
+    str = str + i%2
+
+    return str
+
+} 
+
+//5 '10101'
+
+function consecutive (arr) {
+    for (let i=0; i<arr.length; i++){
+        if (arr[i] + 1 != arr[i+1]) {
+            break
+            return ('not consecutive') 
+    } else {
+        return ('all good')
+    }
 }
 
-multples (2, 8)
+consecutive ([1,2,3,6,11])
