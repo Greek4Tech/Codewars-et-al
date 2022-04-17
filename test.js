f044bbdc1dcab6b4a6c371974ebfd8a4bdbe1714
Greek4Tech <leonidas.gkimisis@gmail.com>
2022-04-16 21:45:43 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,9 @@
-//3 and 9
-function mults (num1, num2) {
-    let arr =[]
-    //i=0 => 3+3
-    for (let i=num1; i<=num2; i=+num1) {
+function multples (num1, num2) {
+    let result = []
+    for (let i=num1; i<=num2; i = num1 + num1){ 
+        result.push(i)
+        }
+        return result
+}
 
-        //add that index into the arr
-        arr.push(i)
-      console.log(arr)  
-    } 
-} 
-
-mults (3, 9)
+multples (2, 8)
