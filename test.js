a02d5285c2823b6f9f2cfb3e30f72d505ee2ded5
Greek4Tech <leonidas.gkimisis@gmail.com>
2022-04-16 21:12:59 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,13 @@
-function zeroOne(num) {
-    newArr = []
-        //return 1,0,1,0
-            //for loop [ind0,ind1,ind2,ind3,] => 
-                //for 0/2 = 0 (0); 1/2 = 0.5 (0+); 2/2 = 1 (0)
-                    for (let i=0; i<=num; i++) {
-                        if (i%2 == 0) {
-                            //we want this to be one
-                            newArr.push(arr(i))
-                        } else {
-                            //we want this to be 0
-                            newArr.push(arr(i))
-                        }
-                    }
-}
+//3 and 9
+function mults (num1, num2) {
+    let arr =[]
+    //i=0 => 3+3
+    for (let i=num1; i<=num2; i=+num1) {
 
-zeroOne(5)
+        //add that index into the arr
+        arr.push(i)
+      console.log(arr)  
+    } 
+} 
+
+mults (3, 9)
