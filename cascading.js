5bd521a9f55a96ff77241d67a31e8472c0593548
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-04-19 10:18:59 -0700

    changes

--- cascading.js
+++ cascading.js
@@ -1,18 +1,8 @@
-// function eachCons(array, n) { 
-//     let res = []; 
-//         for(let i = 0; i < array.length; i++){ 
-//             res.push(array.slice(i,i + n)); 
-//         } 
-//             return res.filter(e => e.length === n); 
-// }
-
-// cascade ([1,2,3,4,5,6,7], 3)
-
-function cascade (arr, n) {
-    let newArr =[];
-    for (let i=0; i<=n; i++) {
-        newArr.push(arr.slice(i, i+ n))
-    } return newArr.filter(x => x.length ===n)
+function cascade (arr, num) {
+    let newArr = []
+    for (let i=0; i<arr.length; i++) {
+        newArr.push(arr.slice(i,i+num))
+    } return newArr.filter(n => n.length === num)
 }
 
-cascade ([1,2,3,4,5,6,7], 3)
+cascade ([1,2,3,4,5,6,7,8], 3)
--- test.js
+++ test.js
@@ -1,8 +1,10 @@
-function positiveInts (num) {
-  //we want an array
-  let arr = []
-  //we need a loop
-  for (let i=0; i<=num; i++) 
-  arr.push(i)
-  return arr
-  } 
+function cascade (arr, num) {
+  let newArr = []
+  //we want a forloop 
+  for (let i=0; i<arr.length; i++) {
+     newArr = arr.slice(i, num)
+  } return arr
+}
+
+//[5,6,7,8,9,10] num =3 
+//=> 567 8910
