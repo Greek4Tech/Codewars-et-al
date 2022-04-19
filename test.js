76d04cd698bd43329949c1436facf57bf9547987
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-04-18 17:36:46 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,13 @@
 function positiveSum(arr) {
-    
+    //we add (reduce) (a => index; b=>value) => add values if they are greater than 0
     return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
  }
+
+ var summation = function (num) {
+    let result = 0;
+    for (var i = 1; i <= num; i++) {
+      result += i;
+    }
+    
+    return result;
+  }
