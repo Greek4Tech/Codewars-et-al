195a0923dc1a0d161abdc2a0ea8aa1200843d4c9
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-04-22 10:41:02 -0700

    Update multiTable.js

--- multiTable.js
+++ multiTable.js
@@ -1,1 +1,7 @@
-function 
+function multTable (num) {
+    if (num <=9 && num >=2){
+    //mult talbe 4x0 = 0 4x1 = 4 4x2=8 etc
+    for (let i=0; i<=num; i++)  
+    console.log (`${num} * ${i} = ${num * i}`)}
+    return 'give a number from 2 to 9'
+}
