cbf0614195b0d2ab91db7aa00489d781949bdcd7
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-04-22 10:25:02 -0700

    KATA and ANKI

--- multiTable.js
+++ multiTable.js
@@ -1,9 +1,1 @@
-let enterNum = Number(prompt("enter num from 2 to 9"))
-if (enterNum >=2 && enterNum <=9) {
-    for (let i=0; i<=enterNum; i++) {
-        console.log(`${enterNum} * ${i} = ${enterNum*i}`)
-    }
-} else {
-    console.log("enter valid entry")
-}
-
+function 
