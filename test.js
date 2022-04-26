5d04f663710489c3b38fb788e51246192053d7eb
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-04-26 09:20:30 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-function century (year) {
-  let result = Math.ceil(year/100)
-  return `it is the ${result} century`
+function returnArr (num) {
+return String(num).split('').map(Number).reserse().join(',')
 }
