118afffeb2f59ae8af81320588b5f8e8f34fa632
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-04-26 10:11:04 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
-function returnArr (num) {
-return String(num).split('').map(Number).reserse().join(',')
+function botScore () {
+  if(!localStorage.getItem('botScore')){
+    localStorage.setItem('botScore', 0)
+  }
+
+  document.querySelector('button').addEventListener('click', anotherOne)
+
+  function anotherOne () {
+  let botScore = Number(localStorage.getItem('botScore'))
+  botScore = botScore + 1
+  localStorage.setItem('botScore', botScore)
+  }
 }
