541b489b95db3dbbc37d5adc1ea2bfa866bef636
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-04-25 17:36:27 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,3 @@
-function returnStuff (num) {
-  let str = ''
-  for (let i=0; i<=num; i++)
-  str=str + i%2
-  return str
-
+function invert(array) {
+  return array.map( x => x === 0 ? x : -x);
 }
-
