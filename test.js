bb75d468509941c62e93263b4288bd6c253926b3
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-04-27 10:20:23 -0700

    new changes

--- test.js
+++ test.js
@@ -1,34 +1,12 @@
-//Arrays
-//Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
-
-function reservePocs (arr) {
-  return arr.reverse()
+function takeInArr (arr) {
+  let newArr = []
+  //even number number%2=0
+  arr.forEach(x => 
+      {
+          if (x%2===0) {
+              newArr.push(x)
+            }
+  
+  })
+  return newArr
 }
-
-//Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
-
-function comparison (arrA, arrB) {
-  return arrA.reduce((a,b)=>a+b*b,0) > arrB.reduce((a,b)=>a+b*b*b,0)  
-}
-
-//Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
-// Some cases:
-// [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
-// [68, -1, 1, -7, 10, 10] => [-1, 10]
-
-//consisting == filter
-
-function multIndex (arr) {
-  return arr.filter ((element, index) => element%index===0)
-}
-
-multIndex ([68, -1, 1, -7, 10, 10])
-
-
-//Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
-
-function sortOut (arr) {
-  return arr.reduce((a,b,) => a + Number(b),0)
-}
-
-sortOut(['1', 2, '3', 4, '5'])
