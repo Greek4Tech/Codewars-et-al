c7d8c5cd9a7e2ceadfc7e4d5367e56a2d8a56204
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-04-28 09:55:13 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
-function takeNumReverse (num) {
-  return String(num).split('').reverse().map(Number)
+// each_cons([1,2,3,4], 2)
+//   #=> [[1,2], [2,3], [3,4]]
+
+// each_cons([1,2,3,4], 3)
+//   #=> [[1,2,3],[2,3,4]]
+
+
+function cascade (arr, num) {
+  let result = []
+  for (let i=0; i<arr.length; i++){
+    result.push(arr.slice(i,i+num))
+  }
+  return result.filter(x => x.length ===num)
 }
