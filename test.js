68cad0d68b407d10befe8c395c8b54b1982c27cd
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-04-28 09:23:51 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,7 @@
-function takeInArr (arr) {
-  let newArr = []
-  //even number number%2=0
-  arr.forEach(x => 
-      {
-          if (x%2===0) {
-              newArr.push(x)
-            }
-  
-  })
-  return newArr
+function multies (n,m) {
+  let arr = []
+    for (let i=n; i<=m; i=i+n){
+    arr.push(i) }
+    
+  return arr
 }
