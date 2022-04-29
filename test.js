4a3e15380cfe87f17b8f42a84473747419607f9b
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-04-29 08:23:55 -0700

    Update test.js

--- test.js
+++ test.js
@@ -11,3 +11,11 @@ function returnSum (arr) {
   return arr.reduce((x,y) => x + y*y,0)
 }
 
+//n=5 --> [5,4,3,2,1]
+
+function opposite (num) {
+  let arr = []
+  for (let i=num; i<=1; i--) {
+    arr.push(i)
+  } return arr
+}
