a46fa475a108576279c06bd3fcc7ce085e2463df
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-05-07 07:11:44 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
-function multipleOfIndex(array) {
-  return array.filter((num, i) => num % i === 0);
-}
+function divisibleBy(numbers, divisor){
+  let newArr = []
+  for (let i=0; i<numbers.length; i++) {
+    if (numbers[i] % divisor ===0) {
+      newArr.push(numbers[i])
+    }
+  } return newArr
+  }
