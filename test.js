677ded1efdbdd63baa275cb82cd47b8f433a887c
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-05-09 08:22:14 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,8 +1,4 @@
-function divisibleBy(numbers, divisor){
-  let newArr = []
-  for (let i=0; i<numbers.length; i++) {
-    if (numbers[i] % divisor ===0) {
-      newArr.push(numbers[i])
-    }
-  } return newArr
-  }
+function Cent (year) {
+    return Math.ceil(year/100)
+}
+
