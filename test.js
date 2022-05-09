6509b72a550ca04053b6a4d609d141df57117712
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-05-09 09:07:34 -0700

    Update test.js

--- test.js
+++ test.js
@@ -4,3 +4,14 @@ function reverseArr (num) {
 }
 
 let greenEnergy = obj = obj['Fuel Type'] = 'avocado oil'
+
+function evensNums (arr) {
+    let evens = []
+    arr.forEach (n=> n%2===0 ? evens.push(n) : 0)
+    return evens 
+}
+
+function findsWolves (arr) {
+const position = arr.reverse().indexOf('wolf')
+return position === 0 ? alert ('go aways wolvesss') : alert (`Sheep ${position} be aware`)
+}
