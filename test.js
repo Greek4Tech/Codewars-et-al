b356bb7e3d2fb1d40ed97b1ff8f32f7acca3c410
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-05-10 08:51:31 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
 function cascading (arr, num) {
-    
+let newArr = []
+for (let i=0; i<arr.length; i++) {
+    newArr.push(arr.slice(i, i+num))
+} return newArr.filter(n=> n.length===num)
 }
+
+cascading ([0,1,2,3,4,5,6], 3)
