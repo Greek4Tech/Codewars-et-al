8677af6ff341f50e3f6669fb7b052e37e4cdb593
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-05-10 08:39:26 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,3 @@
-function warnSheep (arr) {
-    let position = arr.reverse().indexOf('wolf')
-    return position === 0 ? 'go away wolf' : `care sheep, wolf is behind you`
+function cascading (arr, num) {
+    
 }
-
-// [s, s, s, w, s]
-// [0, 1, 2, 3, 4]
-// [s, w, s, s, s]
-
-//position of wolf is 3
-//reverse is 1 
