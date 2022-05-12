d96267d59a4560369bbadf5cc8b0e1f8d94cfea2
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-05-11 17:09:32 -0700

    Update cascading.js

--- cascading.js
+++ cascading.js
@@ -1,8 +1,21 @@
-function cascade (arr, num) {
+function cascadingSubs (arr, num) {
     let newArr = []
     for (let i=0; i<arr.length; i++) {
         newArr.push(arr.slice(i,i+num))
-    } return newArr.filter(n => n.length === num)
+    } return newArr.filter(n=> n.length === num )
 }
 
-cascade ([1,2,3,4,5,6,7,8], 3)
+
+
+
+
+
+
+// function cascade (arr, num) {
+//     let newArr = []
+//     for (let i=0; i<arr.length; i++) {
+//         newArr.push(arr.slice(i,i+num))
+//     } return newArr.filter(n => n.length === num)
+// }
+
+// cascade ([1,2,3,4,5,6,7,8], 3)
