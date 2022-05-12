62f035b9f8c1a5b96a928eda97dfade5d62a7d28
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-05-11 17:04:23 -0700

    Update sumOfDiffArray.js

--- sumOfDiffArray.js
+++ sumOfDiffArray.js
@@ -1,8 +1,13 @@
 function sumsArr (arr) {
-    arr.sort((a,b) => b - a)
+    arr.sort((a,b) => a - b)
     let sum = 0
     for  (let i = arr.length-1; i>0; i--) {
         sum += arr [i] - arr[i-1]
     }
     return sum 
 }
+
+sumsArr([5,10,15,20])
+
+//sumsArr (arr)
+//sums([5,10,15,20]) => 20 - 15 + 15 - 10 + 10 - 5
