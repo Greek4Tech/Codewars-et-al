30b9205101a4846c2590960eb29242f4a7666272
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-05-19 22:43:11 -0700

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,5 +1,5 @@
-function cascading (arr, num) {
-    let newArr=[]
-    for (let i=0; i<arr.length; i++) {
-        newArr.push(arr.slice(i, i+num))
-    } return newArr.filter(n=> n.length===num)}
+// function cascading (arr, num) {
+//     let newArr=[]
+//     for (let i=0; i<arr.length; i++) {
+//         newArr.push(arr.slice(i, i+num))
+//     } return newArr.filter(n=> n.length===num)}
