d1905c7fe696cb8d65403c0ad34c10bf2162f419
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-05-23 11:01:19 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,23 +1,5 @@
-//scores
-
-function scores (arr) {
-    let score = 0
-    for (let i = 0 ; i< arr.length; i++) {
-        if (arr[0] > arr[2]) {
-            score = score + 3
-        } else if (arr[0] === arr[2]) {
-            score = score + 1
-        } else {
-            score = score + 0 
-        } return score
-    }
+function charSequence (str) {
+    if (str = '' || str.length ===0) {
+        return Null
+    } str.split('').splice(1,-1).join(' ')
 }
-
-function totalsScores (games) {
-    let total = 0 
-    games.map(n=> { 
-        if (n[0]>n[2]) { 
-            total = total + 3 
-            } else if (n[0] === n[2]) {
-                total = total + 1 } }) 
-                return total }
