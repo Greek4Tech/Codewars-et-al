9dccc40a02e4984233bec09e939a2895da42fe00
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-05-25 12:56:44 -0700

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,10 +1,3 @@
-var ops = {
-    'AND': (a, b) => a && b,
-    'OR': (a, b) =>  a || b,
-    'XOR': (a, b) => a !== b
-  }
-  
-  
-  function logicalCalc(array, op){
-    return array.reduce(ops[op]);
-  }
+function toCsvText(array) {
+   return array.join('\n');
+}
