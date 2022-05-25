56565095cfc4316bf9b7ee66a493a05f5a907fe3
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-05-25 15:36:52 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
-function toCsvText(array) {
-   return array.join('\n');
+if (!localStorage.getItem('botScore')) {
+   localStorage.setItem('botScore', 0)
+}
+
+document.querySelector(#id).addEventListener('click', anotherOne)
+
+function anotherOne () {
+   let botScore = Number(getItems('botscore'))
+   botScore = botScore + 1
 }
