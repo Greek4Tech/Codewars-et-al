fbc235db090e28fd61477c114bd3d17fa9fa0df7
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-05-26 15:43:29 -0700

    Update test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+if (!localStorage.getItem('botScore')) {
+   localStorage.setItem('botScore', 0)
+}
+
+document.querySelector(#id).addEventListener('click', anotherOne)
+
+function anotherOne () {
+   let botScore = Number(getItems('botscore'))
+   botScore = botScore + 1
+}
