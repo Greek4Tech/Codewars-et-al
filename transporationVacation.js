3a14fc5b618b559498a455781c056ee9502e070e
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-06-06 10:10:16 -0700

    Update transporationVacation.js

--- transporationVacation.js
+++ transporationVacation.js
@@ -2,8 +2,10 @@ function rentalCarCost(d) {
     let total = 0  
     if (d>=7) {
         total = d * 40 - 50
+        return total
     } if (d>=3) {
         total = d * 40 - 20
+        return total
     } else {
         total = d * 40
     } return total
