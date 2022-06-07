b5a5a10717e8133ad30be4c71e076d971438893e
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-06-07 15:41:24 -0700

    new commit

--- test.js
+++ test.js
@@ -1,10 +1,3 @@
-function findMultiples (num1,num2) {
-//empty array
-let arr = []
-//loop that starts from num1 to num2
-//last i? num1 or i = i + num1 
-for (let i=num1; i<=num2; i+=num1) {
-   //show only num2/i ===0 
-   arr.push(i)
-} return arr
+function shortcut (str) {
+return str.replace(/[a,e,i]/g, '')
 }
