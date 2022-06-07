e75269e78ca375ee69f89d3afff1267a2a30d2c2
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-06-07 14:20:05 -0700

    new commit

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-function takeNum (num) {
-   let str = ""
-   for (let i=0; i<num; i++) {
-      str = str + i%2
-   }
+function findMultiples (num1,num2) {
+//empty array
+let arr = []
+//loop that starts from num1 to num2
+//last i? num1 or i = i + num1 
+for (let i=num1; i<=num2; i+=num1) {
+   //show only num2/i ===0 
+   arr.push(i)
+} return arr
 }
