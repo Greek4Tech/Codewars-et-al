43fa8e212b3d6b51272afc46babda229ffd237aa
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-06-07 15:44:18 -0700

    new commit

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
-function shortcut (str) {
-return str.replace(/[a,e,i]/g, '')
+function sumNums (arr) {
+   //sum 
+   let sum = 0 
+   //loop 
+   for(let i=0; i<arr.length; i++){
+      if (arr[i] > 0) {
+         sum = sum + arr[i]
+      } 
+   } return sum
 }
