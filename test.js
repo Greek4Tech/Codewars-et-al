67cc5ceaf0bb7e09e5591a6d6a18cf740978a0d1
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-06-11 16:51:02 -0700

    Update test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-async function loadJson(url) {
-  let response = await fetch(url)
-      if (response.status == 200) {
-        return response.json();
-      }
-    };
-
-loadJson('https://javascript.info/no-such-user.json')
