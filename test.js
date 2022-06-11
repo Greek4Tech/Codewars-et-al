001f73a7e6d8603ed56dcba6a39f5e47535aed38
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-06-11 07:36:44 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,39 +1,10 @@
-new Promise(function(resolve, reject) {
-
-  setTimeout(() => resolve(1), 1000); // (*)
-
-}).then(function(result) { // (**)
-
-  alert(result); // 1
-  return result * 2;
-
-}).then(function(result) { // (***)
-
-  alert(result); // 2
-  return result * 2;
-
-}).then(function(result) {
-
-  alert(result); // 4
-  return result * 2;
-
-});
-
-let promise = new Promise(function(resolve, reject) {
-  setTimeout(() => resolve(1), 1000);
-});
-
-promise.then(function(result) {
-  alert(result); // 1
-  return result * 2;
-});
-
-promise.then(function(result) {
-  alert(result); // 1
-  return result * 2;s
-});
-
-promise.then(function(result) {
-  alert(result); // 1
-  return result * 2;
-});
+const letter = ['a', 'b', 'c', 'd' , 'b', 'b'];
+let count = {};
+letter.forEach(item => {
+  if (count[item]) {
+    count[item]++ 
+  } else {
+    count[item] = 1
+} 
+})
+console.log(count)
