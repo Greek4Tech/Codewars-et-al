593cb960b651befbe1a6d171814baed70cb87e4e
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-06-13 19:14:19 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 function coloring () {
   for (let i=0; i<table.row.length; i++) {
     let row = table.row[i]
-    row.style.backgroundColor= 'red'
+    row.cells[i]style.backgroundColor= 'red'
   }
 }
