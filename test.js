0f9ccf6eaf0fb89229fd1d6389260ad6261d9c52
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-06-14 10:05:37 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,3 @@
-function coloring () {
-  for (let i=0; i<table.rows.length; i++) {
-    let row = table.rows[i]
-    rows.cell[i].background
-  }
+function alternate (str) {
+  return str.split('').map
 }
