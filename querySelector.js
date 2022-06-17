33c7fe9e63414231fc70fa7dac0601c718129048
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-06-17 12:40:24 -0700

    Create querySelector.js

---
+++ querySelector.js
@@ -0,0 +1,7 @@
+// The table with id="age-table".
+// All label elements inside that table (there should be 3 of them).
+// The first td in that table (with the word “Age”).
+// The form with name="search".
+// The first input in that form.
+// The last input in that form.
+// Open the page table.html in a separate wi
