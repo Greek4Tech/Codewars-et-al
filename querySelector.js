2541355a443046d86e58e1dadad974da8877a8dd
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-06-17 13:06:22 -0700

    Update querySelector.js

--- querySelector.js
+++ querySelector.js
@@ -3,11 +3,15 @@ document.getElementById('age-table')
 
 // All label elements inside that table (there should be 3 of them).
 window['age-table'].getElementsByTagName('label')
+//OR
+window['age-table'].querySelectorAll('label')
 
 // The first td in that table (with the word “Age”).
-
+document.querySelectorAll('tr > td:first-child');
 
 // The form with name="search".
+
+
 // The first input in that form.
 // The last input in that form.
 // Open the page table.html in a separate wi
