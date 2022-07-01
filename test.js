53a15d838143d9af4550e73a97b878e522111751
Greek4Tech <97710680+Greek4Tech@users.noreply.github.com>
2022-07-01 11:38:58 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,15 @@
-function each_cons (arr,num) {
-  let newArr = []
-  for (let i=0; i<arr.length; i++) {
-    newArr = arr.slice(i,i+num)
+function leastLarger (num,ind) {
+  let minNum = num[0]
+  let smallestInd
+  for (let i=0; i<num.length; i++)
+  if (num[i]<=minNum) {
+    minNum = num[i]
+    smallestInd = i
   }
-  return newArr
+  console.log(minNum)
+  if (smallestInd > ind) {
+    return ind
+  } return -1
 }
+
+leastLarger ([4,1,3,5,6],0)
